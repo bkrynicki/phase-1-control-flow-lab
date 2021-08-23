5d1c6470ae9132ca03a7937d1830a1101d826c54
Brandon <bjkrynicki@gmail.com>
2021-08-23 16:22:52 -0500

    commit tree c26e4d4159d3a4842250337ef4266e28783a6812

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanceFeet){
   // Write your code here!
+  if (distanceFeet > 2500){
+    return `No can do.`
+  } else if (distanceFeet < 400){
+      return `This one is on me!`
+  } else if (distanceFeet > 2000){
+      return `I will gladly take your thirty bucks.`
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? "Ok, sounds good." : "No go."
+  
 }
+ternaryCheckCity('NYC')
+ternaryCheckCity('Pittsburgh')
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case 'generous':
+      return `Thank you so much.`
+      
+    case 'not as generous':
+      return `Thank you.`
+      
+    case 'thanks for everything':
+      return `Bye.`
+      
+
+  }
 }
